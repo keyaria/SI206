a267cf900abc40d10f50f9d74bcfb347d78e8f67
Keyaria <keyariaw>
2017-11-23 18:45:52 -0500

    Formatting

--- Final_Project/Final_Project_KW.py
+++ Final_Project/Final_Project_KW.py
@@ -9,10 +9,12 @@ import collections
 import json
 import sqlite3
 from instagram.client import InstagramAPI
-access_token = "189197666.3e5b08b.88c1b4c86c4940a086d79eab5ebed29d"
 
-client_secret = "29a444e5be5f4fe18cb7ff3af8c48da2"
-api = InstagramAPI(access_token=access_token, client_secret=client_secret)
-usr = api.user("pandakeyaria")
-my_usr = usr[0]
-print ('User id is', my_usr.id, 'and name is ', my_usr.username)
+def get_user_insta():
+	access_token = "189197666.3e5b08b.88c1b4c86c4940a086d79eab5ebed29d"
+
+	client_secret = "29a444e5be5f4fe18cb7ff3af8c48da2"
+	api = InstagramAPI(access_token=access_token, client_secret=client_secret)
+	usr = api.user("pandakeyaria")
+	my_usr = usr[0]
+	print ('User id is', my_usr.id, 'and name is ', my_usr.username)
